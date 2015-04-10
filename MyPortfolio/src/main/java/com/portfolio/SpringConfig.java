c20419580853fd52c5a4d3ff07a65197daf83561
Sophia <sophia_ghins@yahoo.com>
2015-04-10 14:15:12 -0700

    Initial Commit

---
+++ MyPortfolio/src/main/java/com/portfolio/SpringConfig.java
@@ -0,0 +1,39 @@
+package com.portfolio;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.portfolio")
+public class SpringConfig extends WebMvcConfigurerAdapter{
+	
+	@Bean
+	public MessageSource messageSource(){
+		ResourceBundleMessageSource msgSrc = new ResourceBundleMessageSource();
+		msgSrc.setBasename("captions");
+		return msgSrc;
+	}
+	
+	@Bean
+	public InternalResourceViewResolver getInternalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/pdfs/**").addResourceLocations("/WEB-INF/pdf/");
+		registry.addResourceHandler("/css/**").addResourceLocations("WEB-INF/css/");
+	}
+
+}
---
+++ MyPortfolio/src/main/java/com/portfolio/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping( value = "/index")
+	public String sayHello(Model model){
+		model.addAttribute("greeting", "Hello World");
+		return "index";
+	}
+	
+}
