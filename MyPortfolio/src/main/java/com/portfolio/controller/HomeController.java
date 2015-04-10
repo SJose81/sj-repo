c32673e43a81479e825a6d86f22d18ab05db5ab2
Sophia <sophia_ghins@yahoo.com>
2015-04-10 14:19:48 -0700

    testing git

--- MyPortfolio/src/main/java/com/portfolio/controller/HomeController.java
+++ MyPortfolio/src/main/java/com/portfolio/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
-	@RequestMapping( value = "/index")
+	@RequestMapping( value = "/home")
 	public String sayHello(Model model){
 		model.addAttribute("greeting", "Hello World");
 		return "index";
